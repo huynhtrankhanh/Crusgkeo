7816703b2a412f58732f20bdc18060fb7448c8af
Candy Crush Clone development team <candycrushclone@projects.hcmiu.edu.vn>
2023-03-27 08:39:36 +0000

    Abstract shape drawing functions into a separate class

---
+++ src/DrawShapes.ts
@@ -0,0 +1,97 @@
+class DrawShapes {
+  #shapeSize: number;
+  #context: CanvasRenderingContext2D;
+  constructor(context: CanvasRenderingContext2D, shapeSize: number) {
+    this.#context = context;
+    this.#shapeSize = shapeSize;
+  }
+  drawCircle = (centerX: number, centerY: number) => {
+    this.#context.lineWidth = 1;
+    this.#context.strokeStyle = "black";
+    this.#context.beginPath();
+    this.#context.arc(centerX, centerY, this.#shapeSize / 2, 0, 2 * Math.PI, false);
+    this.#context.stroke();
+  };
+
+  drawSquare = (centerX: number, centerY: number) => {
+    this.#context.lineWidth = 1;
+    this.#context.strokeStyle = "black";
+    const x = centerX - this.#shapeSize / 2;
+    const y = centerY - this.#shapeSize / 2;
+    this.#context.beginPath();
+    this.#context.strokeRect(x, y, this.#shapeSize, this.#shapeSize);
+    this.#context.stroke();
+  };
+
+  drawDiamond = (centerX: number, centerY: number) => {
+    this.#context.lineWidth = 1;
+    this.#context.strokeStyle = "black";
+    this.#context.beginPath();
+    this.#context.moveTo(centerX, centerY - this.#shapeSize / 2);
+    this.#context.lineTo(centerX + this.#shapeSize / 2, centerY);
+    this.#context.lineTo(centerX, centerY + this.#shapeSize / 2);
+    this.#context.lineTo(centerX - this.#shapeSize / 2, centerY);
+    this.#context.lineTo(centerX, centerY - this.#shapeSize / 2);
+    this.#context.stroke();
+  };
+
+  drawColorBomb = (centerX: number, centerY: number) => {
+    this.#context.lineWidth = 1;
+    this.#context.strokeStyle = "black";
+    this.#context.translate(centerX, centerY);
+    this.#context.beginPath();
+
+    const rayCount = 10;
+    for (let i = 0; i < rayCount; i++) {
+      const radius = this.#shapeSize / 2;
+
+      this.#context.moveTo(0, (2 * radius) / 3);
+      this.#context.lineTo(0, radius);
+      this.#context.moveTo(0, 0);
+      this.#context.rotate((2 * Math.PI) / rayCount);
+    }
+
+    this.#context.stroke();
+    this.#context.translate(-centerX, -centerY);
+  };
+
+  drawVerticalStripes = (centerX: number, centerY: number) => {
+    this.#context.lineWidth = 3;
+    this.#context.strokeStyle = "black";
+    this.#context.translate(centerX, centerY);
+    this.#context.beginPath();
+
+    const radius = this.#shapeSize / 2 - 10;
+
+    this.#context.moveTo(0, -radius);
+    this.#context.lineTo(0, radius);
+    this.#context.moveTo(10, -radius);
+    this.#context.lineTo(10, radius);
+    this.#context.moveTo(-10, -radius);
+    this.#context.lineTo(-10, radius);
+
+    this.#context.stroke();
+    this.#context.translate(-centerX, -centerY);
+  };
+
+  drawHorizontalStripes = (centerX: number, centerY: number) => {
+    this.#context.lineWidth = 3;
+    this.#context.strokeStyle = "black";
+    this.#context.translate(centerX, centerY);
+    this.#context.beginPath();
+
+    const radius = this.#shapeSize / 2 - 10;
+
+    this.#context.moveTo(-radius, 0);
+    this.#context.lineTo(radius, 0);
+    this.#context.moveTo(-radius, 10);
+    this.#context.lineTo(radius, 10);
+    this.#context.moveTo(-radius, -10);
+    this.#context.lineTo(radius, -10);
+
+    this.#context.stroke();
+    this.#context.translate(-centerX, -centerY);
+  };
+}
+
+export default DrawShapes;
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+import DrawShapes from "./DrawShapes";
 import MousePosition from "./MousePosition";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -10,157 +11,68 @@ canvas.width = width;
 canvas.height = height;
 
 window.addEventListener("resize", () => {
-	width = window.innerWidth;
-	height = window.innerHeight;
-	canvas.width = width;
-	canvas.height = height;
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
 });
 
 const context = canvas.getContext("2d");
-if (context === null)
-    throw new Error("Failure obtaining context");
+if (context === null) throw new Error("Failure obtaining context");
 
-class GameState {
+class GameState {}
 
-}
+class Grid {}
 
-class Grid {
+class CandiesFalling extends GameState {}
 
-}
-
-class CandiesFalling extends GameState {
-
-}
-
-class CandiesIdle extends GameState {
-
-}
+class CandiesIdle extends GameState {}
 
 requestAnimationFrame(function animate() {
-	requestAnimationFrame(animate);
-	
-	context.clearRect(0, 0, width, height);
-	
-	const centerX = width / 2;
-	const centerY = height / 2;
-	
-	const cellWidth = 64;
-	const cellHeight = 64;
-	const rowCount = 3;
-	const columnCount = 10;
-	
-	const shapeSize = 48;
-	
-	const drawCircle = (centerX: number, centerY: number) => {
-		context.lineWidth = 1;
-		context.strokeStyle = "black";
-		context.beginPath();
-		context.arc(centerX, centerY, shapeSize / 2, 0, 2 * Math.PI, false);
-		context.stroke();
-	};
-	
-	const drawSquare = (centerX: number, centerY: number) => {
-		context.lineWidth = 1;
-		context.strokeStyle = "black";
-		const x = centerX - shapeSize / 2;
-		const y = centerY - shapeSize / 2;
-		context.beginPath();
-		context.strokeRect(x, y, shapeSize, shapeSize);
-		context.stroke();
-	};
-	
-	const drawDiamond = (centerX: number, centerY: number) => {
-		context.lineWidth = 1;
-		context.strokeStyle = "black";
-		context.beginPath();
-		context.moveTo(centerX, centerY - shapeSize / 2);
-		context.lineTo(centerX + shapeSize / 2, centerY);
-		context.lineTo(centerX, centerY + shapeSize / 2);
-		context.lineTo(centerX - shapeSize / 2, centerY);
-		context.lineTo(centerX, centerY - shapeSize / 2);
-		context.stroke();
-	};
-	
-	const drawColorBomb = (centerX: number, centerY: number) => {
-		context.lineWidth = 1;
-		context.strokeStyle = "black";
-		context.translate(centerX, centerY);
-		context.beginPath();
-		
-		const rayCount = 10;
-		for (let i = 0; i < rayCount; i++) {
-			const radius = shapeSize / 2;
-		
-			context.moveTo(0, 2 * radius / 3);
-			context.lineTo(0, radius);
-			context.moveTo(0, 0);
-			context.rotate(2 * Math.PI / rayCount);
-		}
+  requestAnimationFrame(animate);
 
-		context.stroke();
-		context.translate(-centerX, -centerY);
-	};
-	
-	const drawVerticalStripes = (centerX: number, centerY: number) => {
-		context.lineWidth = 3;
-		context.strokeStyle = "black";
-		context.translate(centerX, centerY);
-		context.beginPath();
-		
-		const radius = shapeSize / 2 - 10;
-		
-		context.moveTo(0, -radius);
-		context.lineTo(0, radius);
-		context.moveTo(10, -radius);
-		context.lineTo(10, radius);
-		context.moveTo(-10, -radius);
-		context.lineTo(-10, radius);
-		
-		context.stroke();
-		context.translate(-centerX, -centerY);
-	};
-	
-	const drawHorizontalStripes = (centerX: number, centerY: number) => {
-		context.lineWidth = 3;
-		context.strokeStyle = "black";
-		context.translate(centerX, centerY);
-		context.beginPath();
-		
-		const radius = shapeSize / 2 - 10;
-		
-		context.moveTo(-radius, 0);
-		context.lineTo(radius, 0);
-		context.moveTo(-radius, 10);
-		context.lineTo(radius, 10);
-		context.moveTo(-radius, -10);
-		context.lineTo(radius, -10);
-		
-		context.stroke();
-		context.translate(-centerX, -centerY);
-	};
-	
-	for (let row = 0; row < rowCount; row++)
-		for (let column = 0; column < columnCount; column++) {
-			context.lineWidth = 1;
-			context.strokeStyle = "black";
-			const x = centerX - ((cellWidth * columnCount) / 2) + (column * cellWidth);
-			const y = centerY - ((cellWidth * rowCount) / 2) + (row * cellWidth);
-			context.strokeRect(x, y, cellWidth, cellHeight);
-			
-			const remainder = (row + column) % 4;
-			if (remainder === 0) {
-				drawCircle(x + cellWidth / 2, y + cellWidth / 2);
-				drawVerticalStripes(x + cellWidth / 2, y + cellWidth / 2);
-			}
-			else if (remainder === 1) {
-				drawSquare(x + cellWidth / 2, y + cellWidth / 2);
-				drawHorizontalStripes(x + cellWidth / 2, y + cellWidth / 2);
-			}
-			else if (remainder === 2)
-				drawDiamond(x + cellWidth / 2, y + cellWidth / 2);
-			else if (remainder === 3)
-				drawColorBomb(x + cellWidth / 2, y + cellWidth / 2);
-		}
+  context.clearRect(0, 0, width, height);
+
+  const centerX = width / 2;
+  const centerY = height / 2;
+
+  const cellWidth = 64;
+  const cellHeight = 64;
+  const rowCount = 3;
+  const columnCount = 10;
+
+  const shapeSize = 48;
+
+  const {
+    drawCircle,
+    drawSquare,
+    drawDiamond,
+    drawColorBomb,
+    drawVerticalStripes,
+    drawHorizontalStripes,
+  } = new DrawShapes(context, shapeSize);
+
+  for (let row = 0; row < rowCount; row++)
+    for (let column = 0; column < columnCount; column++) {
+      context.lineWidth = 1;
+      context.strokeStyle = "black";
+      const x = centerX - (cellWidth * columnCount) / 2 + column * cellWidth;
+      const y = centerY - (cellWidth * rowCount) / 2 + row * cellWidth;
+      context.strokeRect(x, y, cellWidth, cellHeight);
+
+      const remainder = (row + column) % 4;
+      if (remainder === 0) {
+        drawCircle(x + cellWidth / 2, y + cellWidth / 2);
+        drawVerticalStripes(x + cellWidth / 2, y + cellWidth / 2);
+        drawColorBomb(x + cellWidth / 2, y + cellWidth / 2);
+      } else if (remainder === 1) {
+        drawSquare(x + cellWidth / 2, y + cellWidth / 2);
+        drawHorizontalStripes(x + cellWidth / 2, y + cellWidth / 2);
+      } else if (remainder === 2)
+        drawDiamond(x + cellWidth / 2, y + cellWidth / 2);
+      else if (remainder === 3)
+        drawColorBomb(x + cellWidth / 2, y + cellWidth / 2);
+    }
 });
 
 export {};
