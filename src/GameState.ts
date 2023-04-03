f32b2c7c39fb10eaab2d795dad6d71e7c2d5e5f2
Candy Crush Clone development team <candycrushclone@projects.hcmiu.edu.vn>
2023-04-03 05:56:00 +0000

    Add GameStateManager

--- src/GameState.ts
+++ src/GameState.ts
@@ -1,4 +1,4 @@
-import { Board } from "./Board";
+import { Board, generateBoardWithoutMatches } from "./Board";
 
 type Effect = undefined; // sorry let's stub this out for now
 
@@ -6,6 +6,8 @@ type GameState =
   | {
       board: Board;
       type: "nothing";
+      // when the user holds a cell and drags to another cell, the two cells are swapped and then the game state transitions back to "nothing". however this doesn't mean when the user continues moving the cursor we should continue swapping immediately. the user must release the mouse in order to perform a new swap
+      mouseNotReleasedYet: boolean;
     }
   | {
       board: Board;
@@ -14,7 +16,7 @@ type GameState =
     }
   | {
       board: Board;
-      type: "animate swap" | "revert swap";
+      type: "animate swap" | "reject swap";
       heldCell: { row: number; column: number };
       swappedWith: { row: number; column: number };
       animateProgress: number;
@@ -25,3 +27,30 @@ type GameState =
       effects: Effect[];
       animateProgress: number;
     };
+
+class GameStateManager {
+  state: GameState;
+  constructor(rowCount: number, columnCount: number) {
+    this.state = {
+      board: generateBoardWithoutMatches(columnCount, rowCount),
+      type: "nothing",
+      mouseNotReleasedYet: false,
+    };
+  }
+  releaseMouse() {
+    if (this.state.type === "cell held") {
+      this.state = {
+        board: this.state.board,
+        mouseNotReleasedYet: false,
+        type: "nothing",
+      };
+      return;
+    }
+    if (this.state.type === "nothing") {
+      this.state.mouseNotReleasedYet = false;
+      return;
+    }
+  }
+}
+
+export default GameStateManager;
