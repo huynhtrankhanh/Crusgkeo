749ce657849b96241e5fd8e16fb37a34f9d35168
Candy_Crush_development_team <candycrushclone@projects.hcmiu.edu.vn>
2023-04-18 19:39:39 +0700

    Change Texture file to PNG to improve performance

--- src/textures.ts
+++ src/textures.ts
@@ -1,18 +1,18 @@
-import boardTextureURL from "../Textures/BoardTexture.svg";
-import circleCandyURL from "../Textures/CircleCandy.svg";
-import squareCandyURL from "../Textures/SquareCandy.svg";
-import diamondCandyURL from "../Textures/DiamondCandy.svg";
+import boardTexturePNG from ;
+import circleCandyPNG from ;
+import squareCandyPNG from ;
+import diamondCandyPNG from ;
 
-const makeImageObject = (url: string) => {
+const makeImageObject = (png: string) => {
   const image = new Image();
-  image.src = url;
+  image.src = png;
   return image;
 };
 
-export const boardTexture = makeImageObject(boardTextureURL);
-export const circleCandy = makeImageObject(circleCandyURL);
-export const squareCandy = makeImageObject(squareCandyURL);
-export const diamondCandy = makeImageObject(diamondCandyURL);
+export const boardTexture = makeImageObject(boardTexturePNG);
+export const circleCandy = makeImageObject(circleCandyPNG);
+export const squareCandy = makeImageObject(squareCandyPNG);
+export const diamondCandy = makeImageObject(diamondCandyPNG);
 
 const waitOnLoad = (image: HTMLImageElement) =>
   new Promise((resolve) => {
