8fa7726d9a5a926c69b0db40b67580b8899391dd
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-04-28 17:07:09 +0700

    Use multiple design patterns in MousePosition
    1. The `IEventHandler` interface is used to define a common structure for event handling classes.
    2. The `MouseEventHandler` and `TouchEventHandler` classes implement the `IEventHandler` interface, demonstrating the **Strategy pattern**.
    3. The `MouseButtonHandler` is an abstract class that defines the structure for handling mouse button events, with the `MouseDownHandler` and `MouseUpHandler` classes extending it, demonstrating the **Template Method pattern**.
    4. The `MousePosition` class now uses instances of these handler classes and delegates the event handling to them, demonstrating the **Delegation pattern**.

--- src/MousePosition.ts
+++ src/MousePosition.ts
@@ -1,43 +1,81 @@
+interface IEventHandler {
+  handle(event: MouseEvent | TouchEvent): void;
+}
+
+class MouseEventHandler implements IEventHandler {
+  constructor(private mousePosition: MousePosition) {}
+
+  handle(event: MouseEvent | TouchEvent): void {
+    if (event instanceof MouseEvent) {
+      this.mousePosition.x = event.clientX;
+      this.mousePosition.y = event.clientY;
+    }
+  }
+}
+
+class TouchEventHandler implements IEventHandler {
+  constructor(private mousePosition: MousePosition) {}
+
+  handle(event: TouchEvent): void {
+    if (event.touches.length < 1) return;
+    this.mousePosition.x = event.touches[0].clientX;
+    this.mousePosition.y = event.touches[0].clientY;
+  }
+}
+
+abstract class MouseButtonHandler {
+  constructor(protected mousePosition: MousePosition) {}
+
+  abstract handle(event: MouseEvent): void;
+}
+
+class MouseDownHandler extends MouseButtonHandler {
+  handle(event: MouseEvent): void {
+    this.mousePosition.leftButtonHeld = true;
+    this.mousePosition.x = event.clientX;
+    this.mousePosition.y = event.clientY;
+  }
+}
+
+class MouseUpHandler extends MouseButtonHandler {
+  handle(event: MouseEvent): void {
+    this.mousePosition.leftButtonHeld = false;
+    this.mousePosition.x = event.clientX;
+    this.mousePosition.y = event.clientY;
+  }
+}
+
 class MousePosition {
   x: number = 0;
   y: number = 0;
   leftButtonHeld: boolean = false;
-  constructor(canvas: HTMLCanvasElement) {
-    canvas.addEventListener("mousedown", (event) => {
-      this.leftButtonHeld = true;
-      this.x = event.clientX;
-      this.y = event.clientY;
-    });
-    canvas.addEventListener("mouseup", (event) => {
-      this.leftButtonHeld = false;
-      this.x = event.clientX;
-      this.y = event.clientY;
-    });
-    canvas.addEventListener("mousemove", (event) => {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    });
 
+  private mouseEventHandler: IEventHandler;
+  private touchEventHandler: IEventHandler;
+  private mouseDownHandler: MouseButtonHandler;
+  private mouseUpHandler: MouseButtonHandler;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.mouseEventHandler = new MouseEventHandler(this);
+    this.touchEventHandler = new TouchEventHandler(this);
+    this.mouseDownHandler = new MouseDownHandler(this);
+    this.mouseUpHandler = new MouseUpHandler(this);
+
+    canvas.addEventListener("mousedown", (event) => this.mouseDownHandler.handle(event));
+    canvas.addEventListener("mouseup", (event) => this.mouseUpHandler.handle(event));
+    canvas.addEventListener("mousemove", (event) => this.mouseEventHandler.handle(event));
     canvas.addEventListener("touchstart", (event) => {
       this.leftButtonHeld = true;
-      if (event.touches.length < 1) return;
-      this.x = event.touches[0].clientX;
-      this.y = event.touches[0].clientY;
+      this.touchEventHandler.handle(event);
     });
     canvas.addEventListener("touchcancel", () => {
       this.leftButtonHeld = false;
     });
     canvas.addEventListener("touchend", (event) => {
       this.leftButtonHeld = false;
-      if (event.touches.length < 1) return;
-      this.x = event.touches[0].clientX;
-      this.y = event.touches[0].clientY;
+      this.touchEventHandler.handle(event);
     });
-    canvas.addEventListener("touchmove", (event) => {
-      if (event.touches.length < 1) return;
-      this.x = event.touches[0].clientX;
-      this.y = event.touches[0].clientY;
-    });
+    canvas.addEventListener("touchmove", (event) => this.touchEventHandler.handle(event));
   }
 }
 
