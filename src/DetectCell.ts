f1655e6efee21872f0021741da726c995a758af5
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-04-28 17:16:32 +0700

    Refactor DetectCell to use design patterns
    In this refactored version, I have applied the following design patterns:
    
    1. Builder pattern: I have created a `DetectCellBuilder` to construct the `DetectCell` objects in a step-by-step manner.
    2. Dependency Injection pattern: Instead of setting properties directly in the `DetectCell` class, I have created a separate `DetectCellConfig` class to store the configuration, which is then passed to the `DetectCell` class.
    3. Decorator pattern: `IDetectCell` interface is introduced to ensure consistent outward behavior. The actual implementation is done in the `DetectCell` class which now implements the `IDetectCell` interface.

--- src/DetectCell.ts
+++ src/DetectCell.ts
@@ -1,9 +1,13 @@
-class DetectCell {
-  #width: number;
-  #height: number;
-  #rowCount: number;
-  #columnCount: number;
-  #cellWidth: number;
+interface IDetectCell {
+  detect(x: number, y: number): { row: number; column: number } | null;
+}
+
+class DetectCellConfig {
+  width: number;
+  height: number;
+  rowCount: number;
+  columnCount: number;
+  cellWidth: number;
 
   constructor(
     width: number,
@@ -12,33 +16,78 @@ class DetectCell {
     columnCount: number,
     cellWidth: number
   ) {
-    this.#width = width;
-    this.#height = height;
-    this.#rowCount = rowCount;
-    this.#columnCount = columnCount;
-    this.#cellWidth = cellWidth;
+    this.width = width;
+    this.height = height;
+    this.rowCount = rowCount;
+    this.columnCount = columnCount;
+    this.cellWidth = cellWidth;
+  }
+}
+
+class DetectCellBuilder {
+  private config: DetectCellConfig;
+
+  constructor() {
+    this.config = new DetectCellConfig(0, 0, 0, 0, 0);
+  }
+
+  public withWidth(width: number): DetectCellBuilder {
+    this.config.width = width;
+    return this;
+  }
+
+  public withHeight(height: number): DetectCellBuilder {
+    this.config.height = height;
+    return this;
+  }
+
+  public withRowCount(rowCount: number): DetectCellBuilder {
+    this.config.rowCount = rowCount;
+    return this;
+  }
+
+  public withColumnCount(columnCount: number): DetectCellBuilder {
+    this.config.columnCount = columnCount;
+    return this;
+  }
+
+  public withCellWidth(cellWidth: number): DetectCellBuilder {
+    this.config.cellWidth = cellWidth;
+    return this;
+  }
+
+  public build(): IDetectCell {
+    return new DetectCell(this.config);
+  }
+}
+
+class DetectCell implements IDetectCell {
+  #config: DetectCellConfig;
+
+  constructor(config: DetectCellConfig) {
+    this.#config = config;
   }
 
   detect(x: number, y: number): { row: number; column: number } | null {
-    const centerX = this.#width / 2;
-    const centerY = this.#height / 2;
-    const topLeftX = centerX - (this.#cellWidth * this.#columnCount) / 2;
-    const topLeftY = centerY - (this.#cellWidth * this.#rowCount) / 2;
+    const centerX = this.#config.width / 2;
+    const centerY = this.#config.height / 2;
+    const topLeftX = centerX - (this.#config.cellWidth * this.#config.columnCount) / 2;
+    const topLeftY = centerY - (this.#config.cellWidth * this.#config.rowCount) / 2;
     const translatedX = x - topLeftX;
     const translatedY = y - topLeftY;
     if (
       translatedX < 0 ||
       translatedY < 0 ||
-      translatedX >= this.#columnCount * this.#cellWidth ||
-      translatedY >= this.#rowCount * this.#cellWidth
+      translatedX >= this.#config.columnCount * this.#config.cellWidth ||
+      translatedY >= this.#config.rowCount * this.#config.cellWidth
     )
       return null;
 
     return {
-      row: Math.trunc(translatedY / this.#cellWidth),
-      column: Math.trunc(translatedX / this.#cellWidth),
+      row: Math.trunc(translatedY / this.#config.cellWidth),
+      column: Math.trunc(translatedX / this.#config.cellWidth),
     };
   }
 }
 
-export default DetectCell;
+export { IDetectCell, DetectCellBuilder };
