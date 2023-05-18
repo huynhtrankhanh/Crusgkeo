eef43dbbcf0ecb2b9138a4e0ee1e2fca02a3b5b4
Candy_Crush_development_team <candycrushclone@projects.hcmiu.edu.vn>
2023-05-18 20:30:25 +0700

    Adding various icons and background

--- src/textures.ts
+++ src/textures.ts
@@ -2,6 +2,12 @@ import boardTexturePNG from "../Textures/BoardTexture.png";
 import circleCandyPNG from "../Textures/CircleCandy.png";
 import squareCandyPNG from "../Textures/SquareCandy.png";
 import diamondCandyPNG from "../Textures/DiamondCandy.png";
+import firstPlacePNG from "../Textures/FirstPlacePNG.png";
+import secondPlacePNG from "../Texture/SecondPlacePNG.png";
+import thirdPlacePNG from "../Texture/ThirdPlacePNG.png";
+import gameBackgroundPNG from "../Textures/GameBackgroundPNG.png";
+import gameBackgroundDarkPNG from "../Textures/GameBackgroundDarkPNG.png";
+
 
 const makeImageObject = (png: string) => {
   const image = new Image();
@@ -13,6 +19,11 @@ export const boardTexture = makeImageObject(boardTexturePNG);
 export const circleCandy = makeImageObject(circleCandyPNG);
 export const squareCandy = makeImageObject(squareCandyPNG);
 export const diamondCandy = makeImageObject(diamondCandyPNG);
+export const firstPlace = makeImageObject(firstPlacePNG);
+export const secondPlace = makeImageObject(secondPlacePNG);
+export const thirdPlace = makeImageObject(thirdPlacePNG);
+export const gameBackground = makeImageObject(gameBackgroundPNG)
+export const gameBackgroundDark = makeImageObject(gameBackgroundDarkPNG);
 
 const waitOnLoad = (image: HTMLImageElement) =>
   new Promise((resolve) => {
@@ -20,5 +31,5 @@ const waitOnLoad = (image: HTMLImageElement) =>
   });
 
 export const waitForAllImages = Promise.all(
-  [boardTexture, circleCandy, squareCandy, diamondCandy].map(waitOnLoad)
+  [boardTexture, circleCandy, squareCandy, diamondCandy, firstPlace, secondPlace, thirdPlace, gameBackground, gameBackgroundDark].map(waitOnLoad)
 );
